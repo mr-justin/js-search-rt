773a31fb92c5cfa86936fded23317ea5cda7f36d
Adam.Eilers <Adam.Eilers@gmail.com>
2012-11-07 05:49:57 +0000

    spaces!

--- js-search-rt/src/svr/js/RadixTreeClassical.js
+++ js-search-rt/src/svr/js/RadixTreeClassical.js
@@ -15,9 +15,9 @@
  *
  */
 function RadixTree() {
-	this.keywordCount = 0;
-	this.dataCount = 0;
-	this.tree = {};
+    this.keywordCount = 0;
+    this.dataCount = 0;
+    this.tree = {};
 }
 
 /***************************************************************************
@@ -41,14 +41,14 @@ function RadixTree() {
  * data = mandatory, new "data" to attach to new/exiting node
  */
 RadixTree.prototype.insert = function insert(key, data) {
-	var index = new Index(this.tree),
-		callbacks = new Callbacks(this._createNode, this._createNode, this._insertData, this._splitNode);
+    var index = new Index(this.tree),
+        callbacks = new Callbacks(this._createNode, this._createNode, this._insertData, this._splitNode);
 
-	//process key
-	key = this._processKey(key);
+    //process key
+    key = this._processKey(key);
 
-	//start recursive insert
-	this._traverse(key, data, index, callbacks);
+    //start recursive insert
+    this._traverse(key, data, index, callbacks);
 }
 
 /*****
@@ -63,12 +63,12 @@ RadixTree.prototype.insert = function insert(key, data) {
  * index = mandatory, Index object used to provide info of the tree
  */
 RadixTree.prototype._insertData = function _insertData(key, data, index) {
-	if (index.node.$ && index.node.$.length) {
-		index.node.$.push(data);
-	} else {
-		index.node.$ = [data];
-	}
-	this.dataCount++;
+    if (index.node.$ && index.node.$.length) {
+        index.node.$.push(data);
+    } else {
+        index.node.$ = [data];
+    }
+    this.dataCount++;
 }
 
 /*****
@@ -83,12 +83,12 @@ RadixTree.prototype._insertData = function _insertData(key, data, index) {
  * index = mandatory, Index object used to provide info of the tree
  */
 RadixTree.prototype._createNode = function _createNode(key, data, index) {
-	if (index.ttlCharsMatch !== 0) {
-		key = key.substr(index.ttlCharsMatch);
-	}
-	index.node[key] = {$:[data]};
-	this.keywordCount++;
-	this.dataCount++;
+    if (index.ttlCharsMatch !== 0) {
+        key = key.substr(index.ttlCharsMatch);
+    }
+    index.node[key] = {$:[data]};
+    this.keywordCount++;
+    this.dataCount++;
 }
 
 /*****
@@ -102,29 +102,29 @@ RadixTree.prototype._createNode = function _createNode(key, data, index) {
  * index = mandatory, Index object used to provide info of the tree
  */
 RadixTree.prototype._splitNode = function _splitNode(key, data, index) {
-	//create new key for existing content
-	var i = index.ttlCharsMatch - index.charsMatch,
-		tempKey = "",
-		tempIndexKey = index.nodeKey.substr(index.charsMatch);
-	tempKey = key.substr(i, index.charsMatch);
+    //create new key for existing content
+    var i = index.ttlCharsMatch - index.charsMatch,
+        tempKey = "",
+        tempIndexKey = index.nodeKey.substr(index.charsMatch);
+    tempKey = key.substr(i, index.charsMatch);
 
-	//add new node to existing index
-	if (index.ttlCharsMatch === key.length) {
-		index.node[tempKey] = {$:[data]};
-	//add new nodes to existing index
-	} else {
-		var str = key.substr(index.ttlCharsMatch);
-		index.node[tempKey] = {};
-		index.node[tempKey][str] = {$:[data]};
-	}
+    //add new node to existing index
+    if (index.ttlCharsMatch === key.length) {
+        index.node[tempKey] = {$:[data]};
+    //add new nodes to existing index
+    } else {
+        var str = key.substr(index.ttlCharsMatch);
+        index.node[tempKey] = {};
+        index.node[tempKey][str] = {$:[data]};
+    }
 
-	//append existing content to new node
-	index.node[tempKey][tempIndexKey] = index.node[index.nodeKey];
+    //append existing content to new node
+    index.node[tempKey][tempIndexKey] = index.node[index.nodeKey];
 
-	//delete existing node
-	delete index.node[index.nodeKey];
-	this.keywordCount++;
-	this.dataCount++;
+    //delete existing node
+    delete index.node[index.nodeKey];
+    this.keywordCount++;
+    this.dataCount++;
 }
 
 /***************************************************************************
@@ -143,14 +143,14 @@ RadixTree.prototype._splitNode = function _splitNode(key, data, index) {
  * data = optional, if no "data" is passed, node will be removed
  */
 RadixTree.prototype.remove = function remove(key, data) {
-	var index = new Index(this.tree),
-		callbacks = new Callbacks(this._removeError, this._removeError, this._removeData, this._removeError);
+    var index = new Index(this.tree),
+        callbacks = new Callbacks(this._removeError, this._removeError, this._removeData, this._removeError);
 
-	//process key
-	key = this._processKey(key);
+    //process key
+    key = this._processKey(key);
 
-	//start recursive removal
-	this._traverse(key, data, index, callbacks);
+    //start recursive removal
+    this._traverse(key, data, index, callbacks);
 }
 
 /*****
@@ -166,41 +166,41 @@ RadixTree.prototype.remove = function remove(key, data) {
  * index = mandatory, Index object used to provide info of the tree
  */
 RadixTree.prototype._removeData = function _removeData(key, data, index) {
-	//if data is undefined, delete node
-	if (typeof data === "undefined") {
-		this.dataCount = this.dataCount - index.node.$.length;
-		delete index.node.$;
-		this.keywordCount--;
-	//else delete data
-	} else {
-		var testData = JSON.stringify(data);
+    //if data is undefined, delete node
+    if (typeof data === "undefined") {
+        this.dataCount = this.dataCount - index.node.$.length;
+        delete index.node.$;
+        this.keywordCount--;
+    //else delete data
+    } else {
+        var testData = JSON.stringify(data);
 
-		//find matching data
-		for (var i = 0, arrlen = index.node.$.length; i < arrlen; i++) {
-			if (testData === JSON.stringify(index.node.$[i])) {
-				break;
-			}
-		}
+        //find matching data
+        for (var i = 0, arrlen = index.node.$.length; i < arrlen; i++) {
+            if (testData === JSON.stringify(index.node.$[i])) {
+                break;
+            }
+        }
 
-		//if match is found, remove data
-		if (i < arrlen) {
-			index.node.$.splice(i,1);
-			this.dataCount--;
-			//if data is empty, perform additional cleanup of node
-			if (index.node.$.length === 0) {
-				delete index.node.$;
-				this.keywordCount--;
-			}
-		//else log error that data doesn't exist
-		} else {
-			console.log("Removal failed. Key: '" + key + "' matched but cound not find matching data to remove.");
-		}
-	}
+        //if match is found, remove data
+        if (i < arrlen) {
+            index.node.$.splice(i,1);
+            this.dataCount--;
+            //if data is empty, perform additional cleanup of node
+            if (index.node.$.length === 0) {
+                delete index.node.$;
+                this.keywordCount--;
+            }
+        //else log error that data doesn't exist
+        } else {
+            console.log("Removal failed. Key: '" + key + "' matched but cound not find matching data to remove.");
+        }
+    }
 
-	//if node is empty, remove empty nodes
-	if (this._isEmpty(index.node)) {
-		this._removeEmptyParents(index.parents);
-	}
+    //if node is empty, remove empty nodes
+    if (this._isEmpty(index.node)) {
+        this._removeEmptyParents(index.parents);
+    }
 }
 
 /*****
@@ -214,17 +214,17 @@ RadixTree.prototype._removeData = function _removeData(key, data, index) {
  * parents = mandatory, Object[] used to remove additional empty nodes
  */
 RadixTree.prototype._removeEmptyParents = function _removeEmptyParents(parents) {
-	//reverse order of parents array to traverse up the tree
-	parents.reverse();
-	//check parents for empty nodes
-	for (var i = 0, arrlen = parents.length; i < arrlen; i++) {
-		for (var str in parents[i]) {
-			//if node is empty, delete it
-			if (this._isEmpty(parents[i][str])) {
-				delete parents[i][str];
-			}
-		}
-	}
+    //reverse order of parents array to traverse up the tree
+    parents.reverse();
+    //check parents for empty nodes
+    for (var i = 0, arrlen = parents.length; i < arrlen; i++) {
+        for (var str in parents[i]) {
+            //if node is empty, delete it
+            if (this._isEmpty(parents[i][str])) {
+                delete parents[i][str];
+            }
+        }
+    }
 }
 
 /*****
@@ -240,23 +240,23 @@ RadixTree.prototype._removeEmptyParents = function _removeEmptyParents(parents) 
  * index = mandatory, Index object used to provide info of the tree
  */
 RadixTree.prototype._removeError = function _removeError(key, data, index) {
-	switch(true) {
-		//if key doesn't exist
-		case (index.ttlCharsMatch === 0):
-			console.log("Removal failed. key: '" + key + "' doesn't exist");
-			break;
-		//if key contains suffix of index.nodeKey
-		case (index.ttlCharsMatch < key.length):
-			console.log("Removal failed. key: '" + key + "' contains a suffix of a matching key: '" + key.substr(0, index.ttlCharsMatch) + "'");
-			break;
-		//if key exists
-		case (index.ttlCharsMatch > 0):
-			console.log("Removal failed. key: '" + key + "' has a partial match until here: '" + key.substr(0, index.ttlCharsMatch) + "'. Check spelling and try removal again");
-			break;
-		//error handling
-		default:
-			console.log("process results failed: " + key);
-	}
+    switch(true) {
+        //if key doesn't exist
+        case (index.ttlCharsMatch === 0):
+            console.log("Removal failed. key: '" + key + "' doesn't exist");
+            break;
+        //if key contains suffix of index.nodeKey
+        case (index.ttlCharsMatch < key.length):
+            console.log("Removal failed. key: '" + key + "' contains a suffix of a matching key: '" + key.substr(0, index.ttlCharsMatch) + "'");
+            break;
+        //if key exists
+        case (index.ttlCharsMatch > 0):
+            console.log("Removal failed. key: '" + key + "' has a partial match until here: '" + key.substr(0, index.ttlCharsMatch) + "'. Check spelling and try removal again");
+            break;
+        //error handling
+        default:
+            console.log("process results failed: " + key);
+    }
 }
 
 /***************************************************************************
@@ -271,8 +271,8 @@ RadixTree.prototype._removeError = function _removeError(key, data, index) {
  *   a flat file to be referenced at runtime on the front end.
  */
 RadixTree.prototype.buildJSONString = function buildJSONString() {
-	//back-end part of web service to return a copy of the tree
-	return JSON.stringify(this.tree, null, 2);
+    //back-end part of web service to return a copy of the tree
+    return JSON.stringify(this.tree, null, 2);
 }
 
 /***************************************************************************
@@ -294,39 +294,39 @@ RadixTree.prototype.buildJSONString = function buildJSONString() {
  * callbacks = mandatory, Callbacks object used to _processResults
  */
 RadixTree.prototype._traverse = function _traverse(key, data, index, callbacks) {
-	var tempKey = key.substr(index.ttlCharsMatch),
-		tempMatch = index.ttlCharsMatch;
+    var tempKey = key.substr(index.ttlCharsMatch),
+        tempMatch = index.ttlCharsMatch;
 
-	//loop through child objects
-	for (var str in index.node) {
-		//loop through characters
-		for (var i = 0, strlen = tempKey.length; i < strlen; i++) {
-			if (tempKey.charAt(i) !== str.charAt(i)) {
-				break;
-			}
-		}
+    //loop through child objects
+    for (var str in index.node) {
+        //loop through characters
+        for (var i = 0, strlen = tempKey.length; i < strlen; i++) {
+            if (tempKey.charAt(i) !== str.charAt(i)) {
+                break;
+            }
+        }
 
-		//if any characters match
-		if (i > 0) {
-			index.charsMatch = i;
-			index.ttlCharsMatch += i;
-			//if exact match, add current node to parents and move to matching node
-			if (i === str.length) {
-				index.parents.push(index.node);
-				index.node = index.node[str];
-			}
-			index.nodeKey = str;
-			break;
-		}
-	}
+        //if any characters match
+        if (i > 0) {
+            index.charsMatch = i;
+            index.ttlCharsMatch += i;
+            //if exact match, add current node to parents and move to matching node
+            if (i === str.length) {
+                index.parents.push(index.node);
+                index.node = index.node[str];
+            }
+            index.nodeKey = str;
+            break;
+        }
+    }
 
-	//if entire key hasn't been checked and there is more tree to search and matching node chars equals node length and total chars match has increased
-	if (index.ttlCharsMatch < key.length && this._leafCount(index.node) && index.charsMatch === index.nodeKey.length && tempMatch !== index.ttlCharsMatch) {
-		this._traverse(key, data, index, callbacks);
-	//else process results
-	} else {
-		this._processResults(key, data, index, callbacks);
-	}
+    //if entire key hasn't been checked and there is more tree to search and matching node chars equals node length and total chars match has increased
+    if (index.ttlCharsMatch < key.length && this._leafCount(index.node) && index.charsMatch === index.nodeKey.length && tempMatch !== index.ttlCharsMatch) {
+        this._traverse(key, data, index, callbacks);
+    //else process results
+    } else {
+        this._processResults(key, data, index, callbacks);
+    }
 }
 
 /*****
@@ -342,27 +342,27 @@ RadixTree.prototype._traverse = function _traverse(key, data, index, callbacks) 
  * callbacks = mandatory, Callbacks object used to _processResults
  */
 RadixTree.prototype._processResults = function _processResults(key, data, index, callbacks) {
-	switch(true) {
-		//if key doesn't exist
-		case (index.ttlCharsMatch === 0):
-			callbacks.nonExists.call(this, key, data, index);
-			break;
-		//if key contains suffix of index.nodeKey
-		case (index.ttlCharsMatch < key.length && index.charsMatch === index.nodeKey.length):
-			callbacks.suffix.call(this, key, data, index);
-			break;
-		//if there is an exact match
-		case (index.ttlCharsMatch === key.length && index.charsMatch === index.nodeKey.length):
-			callbacks.exact.call(this, key, data, index);
-			break;
-		//if key exists
-		case (index.ttlCharsMatch > 0):
-			callbacks.exists.call(this, key, data, index);
-			break;
-		//error handling
-		default:
-			console.log("Process results failed: " + key);
-	}
+    switch(true) {
+        //if key doesn't exist
+        case (index.ttlCharsMatch === 0):
+            callbacks.nonExists.call(this, key, data, index);
+            break;
+        //if key contains suffix of index.nodeKey
+        case (index.ttlCharsMatch < key.length && index.charsMatch === index.nodeKey.length):
+            callbacks.suffix.call(this, key, data, index);
+            break;
+        //if there is an exact match
+        case (index.ttlCharsMatch === key.length && index.charsMatch === index.nodeKey.length):
+            callbacks.exact.call(this, key, data, index);
+            break;
+        //if key exists
+        case (index.ttlCharsMatch > 0):
+            callbacks.exists.call(this, key, data, index);
+            break;
+        //error handling
+        default:
+            console.log("Process results failed: " + key);
+    }
 }
 
 /*****
@@ -376,10 +376,10 @@ RadixTree.prototype._processResults = function _processResults(key, data, index,
  * key = mandatory, String used to process key before being entered/removed from the tree
  */
 RadixTree.prototype._processKey = function _processKey(key) {
-	if (this.keySwap && this.keySwap[key]) {
-		key = this.keySwap[key];
-	}
-	return key.toLowerCase().replace(/ /g,"_");
+    if (this.keySwap && this.keySwap[key]) {
+        key = this.keySwap[key];
+    }
+    return key.toLowerCase().replace(/ /g,"_");
 }
 
 /*****
@@ -392,13 +392,13 @@ RadixTree.prototype._processKey = function _processKey(key) {
  * node = mandatory, Object used to check how many leaf nodes it contains
  */
 RadixTree.prototype._leafCount = function _leafCount(node) {
-	var size = 0;
-	for (var key in node) {
-		if (node.hasOwnProperty(key) && key !== "$") {
-			size++;
-		}
-	}
-	return size;
+    var size = 0;
+    for (var key in node) {
+        if (node.hasOwnProperty(key) && key !== "$") {
+            size++;
+        }
+    }
+    return size;
 }
 
 /*****
@@ -410,7 +410,7 @@ RadixTree.prototype._leafCount = function _leafCount(node) {
  * node = mandatory, Object used to check if node is empty
  */
 RadixTree.prototype._isEmpty = function _isEmpty(node) {
-	return Object.keys(node).length === 0;
+    return Object.keys(node).length === 0;
 }
 
 /***************************************************************************
@@ -430,11 +430,11 @@ RadixTree.prototype._isEmpty = function _isEmpty(node) {
  * parents = mandatory, Object[] used to hold references of all the matching parent nodes
  */
 function Index(node) {
-	this.node = node;
-	this.nodeKey = "";
-	this.charsMatch = 0;
-	this.ttlCharsMatch = 0;
-	this.parents = [];
+    this.node = node;
+    this.nodeKey = "";
+    this.charsMatch = 0;
+    this.ttlCharsMatch = 0;
+    this.parents = [];
 }
 
 /*****
@@ -449,8 +449,8 @@ function Index(node) {
  * exists = mandatory, Function used to reference callback when parts of the key exist in the tree
  */
 function Callbacks(callback1, callback2, callback3, callback4) {
-	this.nonExists = callback1;
-	this.suffix = callback2;
-	this.exact = callback3;
-	this.exists = callback4;
+    this.nonExists = callback1;
+    this.suffix = callback2;
+    this.exact = callback3;
+    this.exists = callback4;
 }
